6b8903b1198b37ad1a41ffeef1ca2b0e2e3bd5a6
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-05-26 13:49:55 +0200

    Added cors to backend to connect with browsers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require("mongoose")
+const cors = require('cors')
 
 let artisanRouter = require("./routes/artisan");
 let clientRouter = require("./routes/client");
@@ -24,6 +25,7 @@ mongoose.connect(db_url);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
